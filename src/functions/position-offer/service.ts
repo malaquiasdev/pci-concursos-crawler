e8d7e2c2e33960c735777b14495bef5989ba40eb
Mateus Malaquias <mateusmalaquiasdev@outlook.com>
2023-01-10 21:15:37 -0300

    feat: crawler job postions by category and log it

---
+++ src/functions/position-offer/service.ts
@@ -0,0 +1,52 @@
+import { URL, positions } from '../../configs/index';
+import { initBrowser } from '../../modules/browser';
+
+export async function crawlerJobPositionOffers() {
+  const jobs: Jobs[] = [];
+  const today = new Date();
+  const browser = await initBrowser();
+  const page = await browser.newPage();
+  for (const position of positions) {
+    const url = getURL(URL.jobPosition, position);
+    await page.goto(url, { waitUntil: 'networkidle0' });
+    const list = await crawlerJobPageElements(page);
+    for (const element of list) {
+      if (new Date(element.date) > today) {
+        jobs.push({
+          ...element,
+          category: position,
+        });
+      }
+    }
+  }
+  console.log(jobs);
+}
+
+function getURL(baseUrl: string, category: string): string {
+  return `${baseUrl}/${category}`;
+}
+
+async function crawlerJobPageElements(page): Promise<Jobs[]> {
+  return page.$$eval('.ca', elements =>
+    elements.map(e => {
+      const title = e.querySelector('a').textContent;
+      const link = e.querySelector('a').href;
+      const date = e.querySelector('div.ce').textContent;
+      const uf = e.querySelector('div.cc').textContent;
+      return {
+        title,
+        link,
+        date,
+        uf,
+      };
+    }),
+  );
+}
+
+interface Jobs {
+  title: string;
+  link: string;
+  date: string;
+  uf: string;
+  category?: string;
+}
